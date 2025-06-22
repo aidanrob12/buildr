3be6467e3c1cbd1bc1b672319685e4baf737e018
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2025-06-21 20:36:01 -0400

    Centering w/o flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import BlogHome from "./BlogHome";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BlogHome />
     </>
   );
 }
---
+++ src/BlogHome/index.tsx
@@ -0,0 +1,48 @@
+export default function BlogHome() {
+  return (
+    <main className="font-sans max-w-132 mx-auto space-y-4 p-4">
+      <header>
+        <h1 className="text-4xl font-bold">Aidan's Blog</h1>
+      </header>
+      <article>
+        <h2 className="text-2xl font-bold mb-1">Post 1</h2>
+        <p className="text-md leading-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+          sodales malesuada risus a scelerisque. Donec vel efficitur eros. Sed
+          fermentum a risus ac molestie. Maecenas at mauris auctor, blandit nunc
+          vel, tincidunt lacus. Integer ut tincidunt quam. Phasellus sit amet
+          dapibus eros, sit amet tincidunt lacus. Nullam non tellus dolor.
+          Suspendisse potenti. Nam vel commodo ipsum. Quisque sed vehicula
+          neque, id sollicitudin magna. Praesent consectetur lobortis velit, sit
+          amet fringilla augue molestie id. Sed non dictum tortor, ut tincidunt
+          ipsum. Donec sit amet ante bibendum, cursus urna nec, ultrices lorem.
+          Suspendisse ut molestie leo. Pellentesque cursus ligula sit amet
+          condimentum consequat. Praesent imperdiet erat nec sapien lacinia
+          feugiat. Nullam dapibus ullamcorper quam ut semper. Sed viverra ligula
+          velit, ut interdum felis euismod sit amet. Vivamus euismod metus a
+          tellus vulputate, non finibus urna dictum. Donec id efficitur purus.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          Phasellus eu consequat dui, vitae ultrices augue. Nullam at
+          condimentum neque. Integer at nulla nec felis maximus dictum. Sed
+          luctus metus id nisl cursus, at maximus massa dictum. Vestibulum
+          feugiat finibus turpis euismod venenatis. Vivamus varius varius metus,
+          vitae accumsan ex sollicitudin vel. Fusce aliquet, neque non elementum
+          dignissim, neque massa tempor neque, a maximus magna erat id massa.
+          Maecenas nec finibus quam. Vestibulum sagittis in odio id egestas.
+          Etiam eget molestie lectus. Ut dolor enim, cursus quis pretium eget,
+          rhoncus ut purus. Aliquam erat volutpat. Nam porttitor dui metus, eu
+          hendrerit orci bibendum in. Pellentesque sed turpis ex. Integer odio
+          nisi, sodales ornare volutpat quis, fermentum eget tellus. Duis dolor
+          dolor, mollis nec nulla at, facilisis vehicula ligula. Donec rhoncus
+          eget risus a vestibulum. Praesent mattis diam nec leo faucibus
+          lobortis. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Suspendisse porttitor leo eget augue
+          eleifend tincidunt. Morbi mattis tincidunt nibh id scelerisque. Nam
+          eleifend eros dolor, eget ornare nulla vestibulum non. Nulla rhoncus
+          nulla ut lectus rutrum scelerisque in ultrices ex. Nunc mattis tempus
+          orci, non faucibus libero malesuada sit amet. Donec eget erat nisi.
+        </p>
+      </article>
+    </main>
+  );
+}
